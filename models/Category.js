eca551305991a9db79ac8f020d93e968a03592e2
RubinoD1 <Rubinod1@gmail.com>
2022-07-31 00:04:02 -0400

    added descriptive comments to connection.js and Category.js

--- models/Category.js
+++ models/Category.js
@@ -9,12 +9,18 @@ Category.init(
     // define columns
   },
   {
+    // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
+     // don't automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
+    // don't pluralize name of database table
     freezeTableName: true,
+     // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
     underscored: true,
+     // make it so our model name stays lowercase in the database
     modelName: 'category',
   }
 );
 
+//export the created model so it can be used in other parts of the app
 module.exports = Category;
