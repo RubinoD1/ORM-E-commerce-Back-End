7f27a764f2638086e4af0f0dcf1580ebd0e82b0f
RubinoD1 <Rubinod1@gmail.com>
2022-07-31 02:52:57 -0400

    second get method added to Category-routes.js with comments for single id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,7 +23,34 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    //using the 'where' option to indicate where we want to find a category where its id value equals whatever req.params.id is
+    // This is similiar to the SQL query: SELECT * FROM category WHERE id = ?
+      where: {
+          id: req.params.id
+      },
+      attributes: ['id', 'category_name'],
+      include: [
+          {
+              model: Product,
+              attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+          }
+      ]
+  })
+      .then(dbCategoryData => {
+        /*Since we are searching by id, it is possible a search could be done for a nonexistent id value. 
+          if the .then() method returns nothing from the query, a 404 status is sent to the client to indicate that it worked
+          but no data was found for that requested id.  */ 
+          if (!dbCategoryData) {
+              res.status(404).json({ message: 'No category found with this id' });
+              return;
+          }
+          res.json(dbCategoryData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
 });
 
 router.post('/', (req, res) => {
